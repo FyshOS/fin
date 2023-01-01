5aca790cffb068fb0b421953e9db44b91d4dba5d
Andy Williams <andy@andy.xyz>
2023-01-01 00:07:08 +0000

    Rotate out old logs when opening a new one

--- log.go
+++ log.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 
 	"fyne.io/fyne/v2"
 )
@@ -20,6 +23,8 @@ func logPath() string {
 
 func openLogWriter() *os.File {
 	path := logPath()
+	rotateLog(path)
+
 	f, err := os.Create(path)
 	if err != nil {
 		fyne.LogError("Unable to open log file", err)
@@ -28,6 +33,24 @@ func openLogWriter() *os.File {
 	log.Println("Logging to", path)
 
 	return f
+}
+
+func rotateLog(path string) {
+	if _, err := os.Stat(path); err != nil && errors.Is(err, os.ErrNotExist) {
+		return
+	}
+
+	crashPath := rotatedLogPath(path)
+	err := os.Rename(path, crashPath)
+	if err != nil {
+		log.Println("Could not save crash file", crashPath)
+	}
+}
+
+func rotatedLogPath(path string) string {
+	parent := filepath.Dir(path)
+	now := time.Now().Format(time.RFC3339)
+	return filepath.Join(parent, fmt.Sprintf("fin-%s.log", now))
 }
 
 func systemLogDir() string {
