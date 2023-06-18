ced8d529a3193a451d02f4e9e2f9359051a2eb0c
Tai Groot <tai@taigrr.com>
2023-06-18 15:39:14 -0400

    Remove bundled background references

--- bundled.go
+++ bundled.go
@@ -6,23 +6,12 @@ import (
 	"fyne.io/fyne/v2"
 )
 
-//go:embed assets/background-dark.png
-var backgroundDark []byte
-//go:embed assets/background-light.png
-var backgroundLight []byte
+
 //go:embed assets/power.svg
 var powerSVG []byte
 //go:embed assets/fysh.png
 var fyshPNG []byte
 
-var resourceBackgroundDarkPng = &fyne.StaticResource{
-	StaticName: "background-dark.png",
-	StaticContent: backgroundDark,
-}
-var resourceBackgroundLightPng = &fyne.StaticResource{
-	StaticName: "background-light.png",
-	StaticContent: backgroundLight,
-}
 var resourcePowerSvg = &fyne.StaticResource{
 	StaticName: "power.svg",
 	StaticContent: powerSVG,
